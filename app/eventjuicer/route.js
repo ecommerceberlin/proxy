bc64b5950045a82c6e5551472dfdc0b78011c9b8
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-12-08 15:47:12 +0100

    Update route.js

--- app/eventjuicer/route.js
+++ app/eventjuicer/route.js
@@ -1,18 +1,39 @@
+import { unstable_cache as cache} from 'next/cache';
+
+/**
+ * 
+ 
+https://nextjs.org/docs/app/api-reference/functions/unstable_cache
+
+*/
+
+export const revalidate = 600
+
+const handleCache = (url) =>  cache(
+    async () => fetch(url, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then(res => res.json()),
+    [url], {
+      "revalidate": 600,
+    //  "tags": []
+    })
 
 
 
 export async function GET(request) {
 
     const { searchParams } = new URL(request.url)
+    const url = searchParams.get("url")
+    
+    if(!url || !url.includes("api.eventjuicer")){
+      return Response.json({"error": "Bad ?url=..."})
+    }
 
-    console.log(searchParams)
+    const data = await handleCache(url)()
 
-    const res = await fetch('https://api.eventjuicer.com/v1/public/hosts', {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-    const data = await res.json()
-   
     return Response.json({ data })
   }
+
+
